39ee1c1be33300e9489a1850ee042542636e8bca
Joe Sasson <ysasson88@gmail.com>
2018-12-19 15:11:23 -0500

    A few fixes (first size, better missing style flow)

--- Gridder.ts
+++ Gridder.ts
@@ -5,12 +5,13 @@ function onOpen(e){
 }
 
 function gridder(){
+  
   // declare spreadsheet, sheet, and data
   let ss = SpreadsheetApp.getActiveSpreadsheet() || SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1CN_IAUYJKLvTiDre_HgD1ayq_o7AXU-ZfDHn4sqi-I0/edit#gid=0')
   let sheet = ss.getActiveSheet()
   let sheetData = sheet.getDataRange().getValues()
   // get list of skus - must be in the first column
-  let styles = sheetData.map(row => row[0]) // -> ['14598-b', '14556-t', ...]
+  let styles = sheetData.map((row, i) => row[0]) // -> ['Sku', '14598-b', '14556-t', ...]
   // get product data from api
   let products = getAllProducts()
   let variants: Object[] = getAllVariants(products)
@@ -24,23 +25,40 @@ function gridder(){
   // map size data per sku - nested objects
   let stockBySku = mapStockBySku(variants, sizeHeaders)
   // set rows via map
+  let badList = []
   let sizeData = sheetData.map((row, i) => {
-    if(i === 0) { return null }
     let style = row[0].toUpperCase()
+    // skip header and blanks
+    if(i === 0 || !style) { return null }
+    if(!stockBySku[style]){
+      badList.push(style)
+    }
     let styleRow = generateStyleRow(style, sizeHeaders, stockBySku)
     return styleRow
   }).filter(row => row)
   // setValues
   let sizeRange = sheet.getRange(2, 3, sizeData.length, sizeData[0].length)
   sizeRange.setValues(sizeData)
+
+  // show errors
+  if(badList.length > 0){
+    const htmlOutput = HtmlService
+    .createHtmlOutput(`<ul>${badList.map(style => `<li>${style}</li>`)}</ul>`)
+    .setTitle('Styles Not Found');
+    SpreadsheetApp.getUi().showSidebar(htmlOutput)
+  }
+  
 }
 
 const generateStyleRow = (style, sizeHeaders, stockBySku) => {
-  if(!stockBySku[style]){ throw new Error(`Style not found: ${style}`)}
-  return sizeHeaders.map((size) => {
-    // map through sizeHeaders and return the qty for each one
-    return stockBySku[style][size]
-  })
+  try {
+    if(!stockBySku[style]){ throw new Error(`Style not found: ${style}`)}
+  }
+  catch (e){
+    return sizeHeaders.map(size => 0)
+  }
+  // map through sizeHeaders and return the qty for each one
+  return sizeHeaders.map(size => stockBySku[style][size])
 }
 
 const mapStockBySku = (variants, sizeHeaders) => {
@@ -54,12 +72,14 @@ const mapStockBySku = (variants, sizeHeaders) => {
     let stockQty = variant.inventory_quantity
     // if the style already exists as a key, set the value for the key of size as stockQty
     if(stockBySku[style]){
-      if(sizeHeaders.indexOf(size) > -1){
+      // if(sizeHeaders.indexOf(size) > -1){
         stockBySku[style][size] = stockQty
-      }
+      // }
     } else {
       // if not, add a key for each sizeHeader within the key that I created for the style
+      // also set the size key to stock quantity
       stockBySku[style] = {...emptySizeRun}
+      stockBySku[style][size] = stockQty
     }
   })
  return stockBySku
@@ -70,8 +90,9 @@ const mapStockBySku = (variants, sizeHeaders) => {
 const getSizeHeaders = variants => {
   // map through all variant objects and get all the sizes
   let sizeCollection = variants.map(variant => {
-    if(!variant.sku){ return }
-    let size = getSizeFromSku(variant.sku)
+    let sku = variant.sku
+    if(!sku){ return }
+    let size = getSizeFromSku(sku)
     return size
   })
   // Then filter out all the duplicates
